ec16760cb982102f3c8b1b3b14445599a4890330
keocorak <corak@wisc.edu>
2023-09-21 16:00:16 -0400

    add necessary data

---
+++ demotrials_configs.R
@@ -0,0 +1,6 @@
+
+## read in data (replace with your file path)
+pedigree_download<-read.csv("crossingdemo_fullped.csv") #needs to be updated each year
+
+studydbid<-"3654" #needs to be updated each year
+
